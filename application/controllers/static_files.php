281710afeb5680248f035f8fafedef9ad5bf338d
Anton Agestam <msn@antonagestam.se>
2010-03-13 19:24:52 +0100

    comment later

---
+++ application/controllers/static_files.php
@@ -0,0 +1,14 @@
+<?php
+	class Static_files extends Controller
+	{
+		public function __construct()
+		{
+			parent::Controller();
+			echo 'eh';
+		}
+		
+		public function Index()
+		{
+			echo 'hej';
+		}
+	}
