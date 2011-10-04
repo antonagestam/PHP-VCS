a2a89219a7b52adb1284b2d8383ef55a4f435d48
Anton Agestam <msn@antonagestam.se>
2011-10-04 03:31:45 +0200

    Continued work with utils.php and edited std_out

--- application/libraries/std_out.php
+++ application/libraries/std_out.php
@@ -2,8 +2,17 @@
 	class std_out{
 		private $out;
 		
-		private function add_out($string){
+		public function add_out($string){
 			$this->out .= $string;
+		}
+		
+		public function clear_out(){
+			$this->out = "";
+		}
+		
+		public function set_out($string){
+			$this->clear_out();
+			$this->add_out($string);
 		}
 		
 		public function get_output(){
@@ -12,5 +21,14 @@
 		
 		public function print_ln($string=NULL){
 			$this->add_out($string."<br/>\n");
+		}
+		
+		public function print_r($array)
+		{
+			ob_start();
+			print_r($array);
+			$ob = ob_get_contents();
+			ob_end_clean();
+			$this->print_ln('<pre>'.$ob.'</pre>');
 		}
 	}
--- application/libraries/utils.php
+++ application/libraries/utils.php
@@ -2,7 +2,24 @@
 	class utils extends std_out{
 		private $ci;
 		
-		public function wget(){
-			$this->print_ln("hej");
+		public function wget($attr){
+			$url = isset($attr['url'])?$attr['url']:@$attr['main'];
+			
+			if(empty($url)){
+				$this->print_ln("error: A URL is required by this command");
+				return;
+			}
+			
+			if($data = @file_get_contents($url)){
+				if(isset($attr['print'])){
+					$this->set_out($data);
+				}else{
+					$path = pathinfo($url);
+					$filename = $path['filename'].$path['extension'];
+					$this->print_ln($filename);
+				}
+			}else{
+				$this->print_ln("error: Could not fetch data from URL");
+			}
 		}
 	}
